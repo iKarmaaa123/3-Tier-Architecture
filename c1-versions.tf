c410108366e059b1d450aefea0be9c63c1f191e7
iKarmaaa123 <waael:hotmail.co.uk>
2023-02-26 04:26:16 +0000

    first commit

---
+++ c1-versions.tf
@@ -0,0 +1,18 @@
+#Terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0.0"
+    }  
+  }
+}
+  
+#Provider Block
+provider "aws" {
+    region = var.aws_region
+}
